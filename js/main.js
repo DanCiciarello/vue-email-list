5e13735724ced49cce0282858fd023f277fd433e
DanCiciarello <me@danieleciciarello.com>
2022-06-17 15:23:28 +0200

    Completed excercise & bonus

--- js/main.js
+++ js/main.js
@@ -14,12 +14,31 @@ new Vue ({
 
     // App data
     data: {
-
+        emailList: [],
+        emailListRendered: [],
     },
 
     // App methods
     methods: {
+        generateEmail() {
+            // Resetto gli array di email
+            this.emailList.splice(0,this.emailList.length);
+            this.emailListRendered.splice(0,this.emailListRendered.length);
+            // Chiamo l'API per generare le mail
+            for (let i = 0; i < 10; i++) {
+                // Genero una mail
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((apiResponse) => {
+                this.emailList.push(apiResponse.data.response);
+                // Controllo se ci sono 10 email
+                if (this.emailList.length === 10) {
+                    this.emailListRendered = this.emailList;
+                };
+                });
+            }
 
+        },
     },
     
 });
